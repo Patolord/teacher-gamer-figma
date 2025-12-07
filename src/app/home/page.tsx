e0aa3a7567520b0650204e1503b568f3957b2382
Rodrigo Luiz Schil Pellegrini <rodrigo_luizp@hotmail.com>
2025-12-07 19:17:36 -0300

    Remove unused import of LandingPage from home page component.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,7 @@ import CalendarBookingSection from "@/components/home/calendar/CalendarBookingSe
 import ContactSection from "@/components/home/contact/ContactSection";
 import CoursesSection from "@/components/home/courses/CoursesSection";
 import HeroSection from "@/components/home/hero-section/HeroSection";
-import LandingPage from "@/components/home/hero-section/LandingPage";
+
 import ResearchSection from "@/components/home/research/ResearchSection";
 import ShopSection from "@/components/home/shop/ShopSection";
 import SubstackSection from "@/components/home/substack/SubstackSection";
